70295440ba6c8bd740e6872e39f9a3ebf2a7cf1e
Terry Lun <A00855225@ad.bcit.ca>
2019-02-27 19:58:39 -0800

    Problem 14 solved

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,12 @@
 package main;
 
 import java.io.IOException;
-
-import problems.Problem13;
 import problems.Problem14;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Problem14.calc();
+
 	}
 }
--- src/problems/Problem14.java
+++ src/problems/Problem14.java
@@ -6,17 +6,34 @@ public class Problem14 {
 	public static void calc() {
 		HashSet<Integer> set = new HashSet<>();
 		int highChain = 0;
-		for (int i = 999999; i > 1 ; i--) {
-			int chain = calcChain(i,set);
-			if (chain > highChain) {
-				highChain = chain;
+		long highNumber = 0;
+		for (long i = 999999L; i > 1 ; i--) {			
+			int temp = (int)i;
+			if (!set.contains(temp)){
+				int count = 0;
+				count = calcChain(i,set);
+				if (count > highChain) {
+					highChain = count;
+					highNumber = i;
+				}
 			}
 		}
-		System.out.println(highChain);
+		System.out.println(highNumber + " with " + highChain);
 	}
 	
-	private static int calcChain(int i, HashSet<Integer> set) {
-		
-		return 0;
+	private static int calcChain(long number, HashSet<Integer> set) {
+		int count=0;
+		while(number!=1) {
+			count++;
+			if(number<=1000000) {
+				set.add((int)(number));
+			}
+			if(number%2==0) {
+				number=number/2;
+			}else {
+				number=number*3+1;
+			}
+		}
+		return count;
 	}
 }
