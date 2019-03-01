58dbcfd4677710ab5634b70f5c3cbb8bea5d8b7d
TerryLun <tianweilun@yahoo.com>
2019-02-28 22:26:38 -0800

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
 import java.io.IOException;
+
 import problems.Problem20;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+
+		long startTime = System.currentTimeMillis();
+
 		Problem20.calc();
 
+		long endTime = System.currentTimeMillis();
+		System.out.println("\nRuntime: " + (endTime - startTime) + "ms");
 	}
 }
