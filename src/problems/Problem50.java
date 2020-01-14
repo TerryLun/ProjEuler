6b66baf5d28e8af08a4dab7610e20e61ad294118
TerryLun <47690039+TerryLun@users.noreply.github.com>
2020-01-14 08:37:21 -0800

    problem 50 wip

--- src/problems/Problem50.java
+++ src/problems/Problem50.java
@@ -9,17 +9,31 @@ public class Problem50 {
 	public static void calc() throws IOException {
 		int[] primeArray = PrimeReader.getPrimeInt();
 		int sum = 0;
-		for(int i =0;i<=545;i++) {
-			sum+=primeArray[i];
+		int maxIndex;
+		int highestCount = 0;
+
+		// determine maxIndex
+		for (maxIndex = 0; sum < 1000000; maxIndex++) {
+			sum += primeArray[maxIndex];
 		}
-		System.out.println(sum);
-		for(int i = 545;i>=0;i--) {
-			sum-=primeArray[i];
-			if(PrimeCheck.check(sum)) {
-				System.out.println(sum);
-				break;
+		// sum = 997661
+		maxIndex -= 2; // =545
+		highestCount = maxIndex + 10;
+
+		// initial window size = 546
+		for (int windowSize = maxIndex - 0 + 1; windowSize < highestCount; windowSize--) {
+			// determine window sum
+			int windowSum = 0;
+			for (int i = 0; i < windowSize; i++) {
+				windowSum += primeArray[i];
+			}
+			// enter inner loop to window slide
+			for(;;) {
+				
 			}
 		}
-		
+s
+		// System.out.println(highestCount);
 	}
+
 }
