7a68dab422eeb571547c87bfb1f6b992227b4562
TerryLun <47690039+TerryLun@users.noreply.github.com>
2020-01-28 10:08:34 -0800

    p50 finished

--- src/problems/Problem50.java
+++ src/problems/Problem50.java
@@ -2,7 +2,6 @@ package problems;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.stream.*;
 
 import prime.PrimeCheck;
 import prime.PrimeReader;
@@ -24,34 +23,41 @@ public class Problem50 {
 		int sum = 0;
 		int maxIndex;
 		int highestCount = 0;
-		
+
 		// determine maxIndex
 		for (maxIndex = 0; sum < 1000000; maxIndex++) {
 			sum += primeArray[maxIndex];
 		}
 		// sum = 997661
-		maxIndex -= 2; // maxIndex=545
+		maxIndex += 10; // maxIndex=567
 		highestCount = 0;
 		boolean found = false;
+		int windowSum = 0;
 
-		// initial window size = 546
-		for (int windowSize = maxIndex + 1; (windowSize < highestCount) || (found == false); windowSize--) {
+		// initial window size = 568
+		for (int windowSize = maxIndex + 1; ((windowSum > highestCount) || (found == false)); windowSize--) {
+			System.out.print(windowSize+" ");
+			System.out.println(maxIndex);
 			// determine window sum
-			int windowSum = 0;
+			windowSum = 0;
 			// enter inner loop to window slide
-			for (int i = 0; i + windowSize < maxIndex + 1; i++) {
+			for (int i = 0; i + windowSize <= maxIndex+1; i++) {
+				System.out.print(i + " ");
 				// calculate window sum (int startInclusive, int endExclusive)
-				windowSum = Arrays.stream(primeArray, i, windowSize).sum(); 
+				windowSum = Arrays.stream(primeArray, i, windowSize).sum();
 				// determine if sum is a prime
 				if (PrimeCheck.check(windowSum)) {
-					highestCount = windowSum;
+					if (highestCount < windowSum) {
+						highestCount = windowSum;
+					}
 					found = true;
-					
 				}
 			}
+			System.out.print("\nWinSize= " + windowSize + " ");
+			System.out.print("WinSum= " + windowSum + " ");
+			System.out.print("HighestCount= " + highestCount + " ");
+			System.out.println(found+"\n");
 		}
-		
-		
 		System.out.println(highestCount);
 	}
 
